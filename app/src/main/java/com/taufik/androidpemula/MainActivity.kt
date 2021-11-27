fe49fdf71ad65d73130b23a7e105a6b417c3d9e3
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-11-27 10:39:40 +0700

    Updating project.

--- app/src/main/java/com/taufik/androidpemula/MainActivity.kt
+++ app/src/main/java/com/taufik/androidpemula/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // call function
+        toDo()
+    }
+
+    private fun toDo() {
+        // TODO 1 : do something
+    }
+
+    private fun todo2(num1: Int, num2: Int): Int {
+        var result: Int
+        result = num1 + num2
+        result = num1 * num2
+        return result
     }
 }
