29be774988755d85a0f8ae7093067768a891e0f8
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-12-11 11:44:09 +0700

    Adding view and view group activity, also its components.

--- app/src/main/java/com/taufik/androidpemula/ViewViewGroupActivity.kt
+++ app/src/main/java/com/taufik/androidpemula/ViewViewGroupActivity.kt
@@ -1,11 +1,29 @@
 package com.taufik.androidpemula
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidpemula.databinding.ActivityViewViewGroupBinding
 
 class ViewViewGroupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityViewViewGroupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_view_group)
+        binding = ActivityViewViewGroupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Google Pixel"
+
+        setOnClickButton()
+    }
+
+    private fun setOnClickButton(){
+        binding.apply {
+            btnBuy.setOnClickListener {
+                Toast.makeText(this@ViewViewGroupActivity, "Beli dong", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
